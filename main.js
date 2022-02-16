55f44283fd2fa37e2c903e5646b4882c5fde1f75
Andrew Head <head.andrewm@gmail.com>
2022-02-16 09:51:56 -0500

    Snapshot at the end of lecture 1

---
+++ main.js
@@ -0,0 +1,5 @@
+// This is a comment
+window.addEventListener("load", function() {
+    // Setting background color (or other styles) from JavaScript takes precedence over setting styles from CSS.
+    document.body.style.backgroundColor = "lightblue";
+});

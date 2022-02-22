04feb1827e50251278a9de3c5adc5151aec39698
Andrew Head <head.andrewm@gmail.com>
2022-02-22 13:42:16 -0500

    Snapshot at the end of lecture 3

--- main.js
+++ main.js
@@ -27,8 +27,38 @@ window.addEventListener("load", function() {
     */
     var myTextArea = document.querySelector("textarea#codemirror-text-area");
     var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
-        value: "function myScript(){return 100;}\n",
+        // value: "function myScript(){return 100;}\n",
         mode:  "javascript",
-        viewportMargin: Infinity,
+        // viewportMargin: Infinity,
     });
+
+    var handle = document.querySelector("#code-editor-handle");
+    var codeEditorContainer = document.querySelector("#code-editor-container");
+    // handle.addEventListener("click", function() {
+    //     console.log("The handle was clicked.");
+    //     // var codeEditorContainer = document.querySelector("#code-editor-container");
+    //     codeEditorContainer.style.left = "150px";
+    //     codeEditorContainer.style.top = "200px";
+    // });
+
+    var isMouseDown = false;
+    document.body.addEventListener("mousedown", function(event) {
+        console.log("1. User pressed down the mouse.");
+        isMouseDown = true;
+    });
+    document.body.addEventListener("mousemove", function(event) {
+        if (isMouseDown) {
+            console.log("2. User moved the mouse.");
+            var newX = event.x;
+            var newY = event.y;
+            codeEditorContainer.style.left = newX;
+            codeEditorContainer.style.top = newY;
+        }
+    });
+    document.body.addEventListener("mouseup", function(event) {
+        console.log("3. User lifted the mouse.");
+        isMouseDown = false;
+    });
+
+    console.log(myCodeMirror);
 });
